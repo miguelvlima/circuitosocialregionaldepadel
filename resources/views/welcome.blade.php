804c0b9034fc1c21f94b801012e80271364fa989
Luis Lima <luis.lima@wrightia.com>
2026-01-13 09:41:48 +0000

    remove images and fix name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Circuito social Regional de Padel</title>
+  <title>Circuito Social Regional de Padel</title>
 
   <style>
     :root{
